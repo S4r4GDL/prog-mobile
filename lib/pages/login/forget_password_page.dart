de7e986d3391e87d5f3a47c5d99032742e05de33
S4R4GDL <saragdl7321@gmail.com>
2025-03-16 19:00:18 -0300

    initial commit - criiação das pages

---
+++ lib/pages/login/forget_password_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ForgetPasswordPage extends StatelessWidget {
+  const ForgetPasswordPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Recupere sua senha');
+  }
+}
---
+++ lib/pages/login/signup_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatelessWidget {
+  const SignupPage({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Realize o cadastro');
+  }
+}
