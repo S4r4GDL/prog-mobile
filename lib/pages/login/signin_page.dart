45387829e46a6858fdcd7c553df2b2f530c22779
S4R4GDL <saragdl7321@gmail.com>
2025-03-17 08:21:34 -0300

    Login-page Criação de tema e de campos para login

--- lib/pages/login/signin_page.dart
+++ lib/pages/login/signin_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:passwordfield/passwordfield.dart';
+import 'package:prog_mobile_app_educadin/theme/theme.dart';
 import 'package:prog_mobile_app_educadin/widgets/login_scaffold.dart';
 
 class SigninPage extends StatefulWidget {
@@ -9,8 +11,125 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
+  final _loginFormKey = GlobalKey<FormState>();
+  bool rememberMe = true;
+
   @override
   Widget build(BuildContext context) {
-    return LoginScaffold(child: Text('Entrar'),);
+    return LoginScaffold(
+      child: Column(
+        children: [
+          Expanded(flex: 1, child: SizedBox(height: 10)),
+          Expanded(
+            flex: 7,
+            child: Container(
+              padding: EdgeInsets.fromLTRB(24, 48, 24, 16),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(40),
+                  topRight: Radius.circular(40),
+                ),
+              ),
+              child: Form(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Acesse sua conta',
+                      style: TextStyle(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: lightColorScheme.primary,
+                      ),
+                    ),
+                    const SizedBox(height: 32),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Entre com o menu';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        label: const Text('Email'),
+                        hintText: 'Insira o seu email',
+                        hintStyle: const TextStyle(color: Colors.black26),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black12),
+                          borderRadius: BorderRadius.circular(10),
+
+                        ),
+                        focusColor: lightColorScheme.primary
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    PasswordField(
+                      color: Colors.blue,
+                      passwordConstraint: r'.*[@$#.*].*',
+                      hintText: 'A senha deve ter caracteres especiais',
+                      border: PasswordBorder(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: lightColorScheme.primary,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: lightColorScheme.primary,
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                          borderSide: BorderSide(
+                            width: 2,
+                            color: lightColorScheme.error,
+                          ),
+                        ),
+                      ),
+                      errorMessage:
+                          'must contain special character either . * @ # \$',
+                    ),
+                    const SizedBox(height: 24),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Checkbox(
+                              value: rememberMe,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  rememberMe = value!;
+                                });
+                              },
+                              activeColor: lightColorScheme.primary,
+                            ),
+
+                            const Text(
+                              'Lembrar-me',
+                              style: TextStyle(color: Colors.black45),
+                            ),
+                          ],
+                        ),
+                        const Text('Esqueceu a senha?',
+                          style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/theme/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+const lightColorScheme = ColorScheme(
+    brightness: Brightness.light,
+    primary: Color(0xFF7ED957),
+    onPrimary: Color(0xFF7AA345),
+    secondary: Color(0xFFC1FF72),
+    onSecondary: Color(0xFF85AF51),
+    error: Color(0xFFFF001D),
+    onError: Color(0xFFFFFFFF),
+    surface: Color(0xFFDAF3D1),
+    onSurface: Color(0xFF181717)
+);
+
+
+//TODO: dark theme
