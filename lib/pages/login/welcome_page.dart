4233ad638e7c602096c671b4634b764f541ffe04
S4R4GDL <saragdl7321@gmail.com>
2025-03-16 23:08:28 -0300

    Welcome-page: Criação de botão estilizável e layout de login default

--- lib/pages/login/welcome_page.dart
+++ lib/pages/login/welcome_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:prog_mobile_app_educadin/pages/login/signin_page.dart';
+import 'package:prog_mobile_app_educadin/pages/login/signup_page.dart';
+import 'package:prog_mobile_app_educadin/widgets/login_scaffold.dart';
+import 'package:prog_mobile_app_educadin/widgets/rounded_costom_button.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -6,6 +10,63 @@ class WelcomePage extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Text('Bem vindo!');
+    return LoginScaffold(
+      child: Column(
+        children: [
+          Flexible(
+            flex: 6,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 40),
+              child: Center(
+                child: RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'Bem vindo de volta!\n',
+                        style: TextStyle(
+                          fontSize: 34.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                        text: '\nEntre com seus dados ou cadastre-se',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Flexible(
+            flex: 4,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                children: [
+                  Expanded(
+                    child: RoundedCostomButton(
+                      buttonColor: Colors.transparent,
+                      buttonText: 'Cadastre-se',
+                      labelColor: Colors.white,
+                      onTap: SignupPage(),
+                    ),
+                  ),
+                  Expanded(
+                    child: RoundedCostomButton(
+                      buttonColor: Colors.white,
+                      buttonText: 'Entrar',
+                      onTap: SigninPage(),
+                      labelColor: Colors.green,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/widgets/rounded_costom_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:prog_mobile_app_educadin/pages/login/signin_page.dart';
+
+class RoundedCostomButton extends StatelessWidget {
+  const RoundedCostomButton({
+    super.key,
+    this.buttonText,
+    this.buttonColor,
+    this.onTap,
+    this.labelColor
+  });
+
+  final String? buttonText;
+  final Color? buttonColor;
+  final Color? labelColor;
+  final Widget? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (e) => onTap!));
+      },
+      child: Container(
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          color: buttonColor ?? Colors.white,
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(50),
+          ),
+        ),
+        child: Text(
+          buttonText!,
+          textAlign: TextAlign.center,
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold,
+          color: labelColor ?? Colors.black),
+        ),
+      ),
+    );
+  }
+}
