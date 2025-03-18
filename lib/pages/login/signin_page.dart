aece6602a57a0323cd861805e7515793c7c8cd5d
S4R4GDL <saragdl7321@gmail.com>
2025-03-17 23:59:37 -0300

    Login-page Criação do botão entrar, rotas de caminhos e estilização dos campos

--- lib/pages/login/signin_page.dart
+++ lib/pages/login/signin_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:passwordfield/passwordfield.dart';
 import 'package:prog_mobile_app_educadin/theme/theme.dart';
 import 'package:prog_mobile_app_educadin/widgets/login_scaffold.dart';
 
@@ -13,6 +12,7 @@ class SigninPage extends StatefulWidget {
 class _SigninPageState extends State<SigninPage> {
   final _loginFormKey = GlobalKey<FormState>();
   bool rememberMe = true;
+  bool _obscurePassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -31,99 +31,191 @@ class _SigninPageState extends State<SigninPage> {
                   topRight: Radius.circular(40),
                 ),
               ),
-              child: Form(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Acesse sua conta',
-                      style: TextStyle(
-                        fontSize: 26,
-                        fontWeight: FontWeight.bold,
-                        color: lightColorScheme.primary,
-                      ),
-                    ),
-                    const SizedBox(height: 32),
-                    TextFormField(
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Entre com o menu';
-                        }
-                        return null;
-                      },
-                      decoration: InputDecoration(
-                        label: const Text('Email'),
-                        hintText: 'Insira o seu email',
-                        hintStyle: const TextStyle(color: Colors.black26),
-                        border: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.black12),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        enabledBorder: OutlineInputBorder(
-                          borderSide: const BorderSide(color: Colors.black12),
-                          borderRadius: BorderRadius.circular(10),
-
-                        ),
-                        focusColor: lightColorScheme.primary
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    PasswordField(
-                      color: Colors.blue,
-                      passwordConstraint: r'.*[@$#.*].*',
-                      hintText: 'A senha deve ter caracteres especiais',
-                      border: PasswordBorder(
-                        border: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: lightColorScheme.primary,
-                          ),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: lightColorScheme.primary,
-                          ),
-                          borderRadius: BorderRadius.circular(12),
-                        ),
-                        focusedErrorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(12),
-                          borderSide: BorderSide(
-                            width: 2,
-                            color: lightColorScheme.error,
-                          ),
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _loginFormKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Acesse sua conta',
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                          color: lightColorScheme.primary,
                         ),
                       ),
-                      errorMessage:
-                          'must contain special character either . * @ # \$',
-                    ),
-                    const SizedBox(height: 24),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: rememberMe,
-                              onChanged: (bool? value) {
-                                setState(() {
-                                  rememberMe = value!;
-                                });
-                              },
-                              activeColor: lightColorScheme.primary,
+                      const SizedBox(height: 32),
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Entre com o email';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          label: const Text('Email'),
+                          hintText: 'Insira o seu email',
+                          hintStyle: const TextStyle(color: Colors.black26),
+                          border: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: const BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
                             ),
-
-                            const Text(
-                              'Lembrar-me',
-                              style: TextStyle(color: Colors.black45),
+                            borderRadius: BorderRadius.circular(16.0),
+                          ),
+                          floatingLabelStyle: const TextStyle(
+                            color: Colors.green,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      TextFormField(
+                        obscureText: _obscurePassword,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Insira sua senha';
+                          } else if (!RegExp(
+                            r'.*[@$#%&.*].*',
+                          ).hasMatch(value)) {
+                            return 'Deve conter caracteres especiais . * @ # \$';
+                          }
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Senha',
+                          hintText: 'Insira sua senha',
+                          hintStyle: const TextStyle(color: Colors.black26),
+                          border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black26),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.green,
+                              width: 2.0,
                             ),
-                          ],
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          floatingLabelStyle: const TextStyle(
+                            color: Colors.green,
+                          ),
+                          errorBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: lightColorScheme.error,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(16),
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              _obscurePassword
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                              color: Colors.black54,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _obscurePassword = !_obscurePassword;
+                              });
+                            },
+                          ),
                         ),
-                        const Text('Esqueceu a senha?',
-                          style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+                      ),
+                      const SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: rememberMe,
+                                onChanged: (bool? value) {
+                                  setState(() {
+                                    rememberMe = value!;
+                                  });
+                                },
+                                activeColor: lightColorScheme.primary,
+                              ),
+                              const Text(
+                                'Lembrar-me',
+                                style: TextStyle(color: Colors.black45),
+                              ),
+                            ],
+                          ),
+                          const Text(
+                            'Esqueceu a senha?',
+                            style: TextStyle(
+                              color: Colors.green,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 32),
+                      SizedBox(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            if (_loginFormKey.currentState!.validate()) {
+                              if (rememberMe) {
+                                ScaffoldMessenger.of(context).showSnackBar(
+                                  const SnackBar(
+                                    content: Text(
+                                      'Encaminha para a home page com lembre-se',
+                                    ),
+                                  ),
+                                );
+                              }
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Encaminha para a home page'),
+                                ),
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text('Dados inválidos!'),
+                                ),
+                              );
+                            }
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            // Change button background color here
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(
+                                16,
+                              ), // Optional: Rounded corners
+                            ),
+                            padding: const EdgeInsets.symmetric(
+                              vertical: 16,
+                            ), // Optional: Adjust padding
+                          ),
+                          child: const Text(
+                            'Entrar',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
