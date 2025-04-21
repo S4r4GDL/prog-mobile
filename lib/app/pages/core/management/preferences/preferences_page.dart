c1e55762a8c40d4305f8883a750767d0ea3df10b
Rafael Guibson <rafaelguibsons@gmail.com>
2025-04-21 17:58:39 -0300

    [Commit 04] - Criada página de preferencias com estrutura incial.

---
+++ lib/app/pages/core/management/preferences/preferences_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../../../widgets/manage_default.dart';
+import '../../../../theme/theme.dart';
+
+class PreferencesPage extends StatefulWidget {
+  const PreferencesPage({super.key});
+
+  @override
+  State<PreferencesPage> createState() => _PreferencesPageState();
+}
+
+class _PreferencesPageState extends State<PreferencesPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
