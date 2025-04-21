77d3d4ec45a7ea2591becc617eb01c2e4ff2694e
Rafael Guibson <rafaelguibsons@gmail.com>
2025-04-21 12:21:04 -0300

    [Commit 02] - Inicio da page expenses.

---
+++ lib/app/pages/core/management/expenses/expenses_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../../../widgets/manage_default.dart';
+import '../../../../theme/theme.dart';
+
+class ExpensesPage extends StatefulWidget {
+  const ExpensesPage({super.key});
+
+  @override
+  State<ExpensesPage> createState() => _ExpensesPageState();
+}
+
+class _ExpensesPageState extends State<ExpensesPage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  
+}
